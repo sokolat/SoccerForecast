49cc207798d460567b3372b72e7743b90cefc168
rango <sidya.galakho@umontreal.ca>
2024-03-03 13:05:34 -0500

    refactored code & added treshold for dropping columns

--- Scripts/data_preparation.py
+++ Scripts/data_preparation.py
@@ -16,23 +16,11 @@ def connect_to_database(file_path):
 def read_data(conn):
     try:
         match_df = pd.read_sql_query("SELECT * FROM Match", conn)
+
         return match_df
     except sqlite3.Error as e:
         print("Error reading data from database:", e)
         return None
-
-
-def check_duplicates(df):
-    is_dup = (~df.duplicated()).all()
-    print(is_dup)
-
-
-def calculate_missing_values(df):
-    percent_missing = df.isnull().sum() * 100 / len(df)
-    missing_value_df = pd.DataFrame({'column_name': df.columns, 'percent_missing': percent_missing})
-    missing_value_df.sort_values('percent_missing', inplace=True, ascending=False)
-    print(missing_value_df)
-
 
 def main():
     # Get the current working directory (where your Python script is located)
@@ -45,8 +33,11 @@ def main():
     if conn is not None:
         match_df = read_data(conn)
         if match_df is not None:
-            check_duplicates(match_df)
-            calculate_missing_values(match_df)
+            if (~match_df.duplicated()).all() :
+                match_df.drop_duplicates()
+                # keeping all columns with at least 90% of non null values
+                match_df = match_df.dropna(thresh=0.9 * len(match_df), axis=1)
+                print(match_df)
             conn.close()
 
 
