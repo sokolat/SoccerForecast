10c2bd0abad0926049674039a344e802dda8fffa
rango <sidya.galakho@umontreal.ca>
2024-03-05 23:29:47 -0500

    split data into train and test

--- Scripts/data_preparation.py
+++ Scripts/data_preparation.py
@@ -1,10 +1,12 @@
 import sqlite3
 import pandas as pd
 import argparse
+from sklearn.model_selection import train_test_split
+
 
 def connect_to_database(file_path):
     """
-    Connect to SQLite database.
+    Connect to the SQLite database.
 
     Parameters:
     - file_path (str): Path to the SQLite database file.
@@ -20,9 +22,10 @@ def connect_to_database(file_path):
         print("Error connecting to SQLite database:", e)
         return None
 
+
 def read_data(conn):
     """
-    Read data from SQLite database tables 'Match' and 'Team_attributes'.
+    Read data from the 'Match' and 'Team_attributes' tables in the database.
 
     Parameters:
     - conn (sqlite3.Connection): SQLite database connection.
@@ -39,9 +42,10 @@ def read_data(conn):
         print("Error reading data from database:", e)
         return None
 
+
 def clean_data(match_df, team_attrb_df, do_normalize=False):
     """
-    Clean and preprocess data.
+    Clean and preprocess the data.
 
     Parameters:
     - match_df (pd.DataFrame): DataFrame containing 'Match' table data.
@@ -52,40 +56,60 @@ def clean_data(match_df, team_attrb_df, do_normalize=False):
     - merged_df (pd.DataFrame): Cleaned and merged DataFrame.
     """
     if all(df is not None for df in [match_df, team_attrb_df]):
+        # Drop unnecessary columns
         match_df.drop(columns=['season', 'date'], axis=1, inplace=True)
         match_df.dropna(thresh=0.9 * len(match_df), axis=1, inplace=True)
         match_df.fillna(match_df.mean(numeric_only=True).round(1), inplace=True)
+
         team_attrb_df.drop(columns=['buildUpPlayDribbling', 'date'], axis=1, inplace=True)
-        df_attrb_home_df = team_attrb_df.add_prefix('home_')
-        merged_home = pd.merge(df_attrb_home_df, match_df, on='home_team_api_id')
-        df_attrb_away_df = team_attrb_df.add_prefix('away_')
-        merged_df = pd.merge(df_attrb_away_df, merged_home, on='away_team_api_id')
+
+        # Merge team attributes for home and away teams with match data
+        df_attrb_home = team_attrb_df.add_prefix('home_')
+        df_attrb_away = team_attrb_df.add_prefix('away_')
+        merged_home = pd.merge(df_attrb_home, match_df, left_on='home_team_api_id', right_on='home_team_api_id')
+        merged_df = pd.merge(df_attrb_away, merged_home, left_on='away_team_api_id', right_on='away_team_api_id')
+
+        # Drop id columns and normalize data if needed
         merged_df.drop(list(merged_df.filter(regex='id')), axis=1, inplace=True)
         if do_normalize:
             cols_to_norm = list(merged_df.filter(regex='^(?:(?!Class).)*$'))
             merged_df[cols_to_norm] = merged_df[cols_to_norm].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+
+        # Create a new column for match outcome and drop goal-related columns
         merged_df['home_outcome'] = merged_df.apply(lambda x: 0 if x['away_team_goal'] > x['home_team_goal'] else (
             1 if x['home_team_goal'] > x['away_team_goal'] else 2), axis=1)
         merged_df.drop(list(merged_df.filter(regex='goal')), axis=1, inplace=True)
+
         return merged_df
     else:
         return None
 
+
 def main():
     """
-    Main function to execute data preprocessing pipeline.
+    Main function to execute the data preprocessing pipeline.
     """
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--do_normalize', action='store_true', help='Normalize the data')
     parser.add_argument('--file_path', type=str, default='database.sqlite', help='Path to the SQLite database file')
     args = parser.parse_args()
+
+    # Connect to the database
     conn = connect_to_database(args.file_path)
     if conn is not None:
+        # Read data from the database
         match_df, team_attrb_df = read_data(conn)
+        # Clean and preprocess the data
         merged_df = clean_data(match_df, team_attrb_df, do_normalize=args.do_normalize)
         if merged_df is not None:
-            merged_df.to_csv('cleaned_data.csv', index=False)
+            # Split the data into train and test sets and save them as CSV files
+            train, test = train_test_split(merged_df, test_size=0.2)
+            merged_df.head(10).to_csv('sample.csv')
+            train.to_csv('cleaned_data_train.csv', index=False)
+            test.to_csv('cleaned_data_test.csv', index=False)
         conn.close()
+
 
 if __name__ == "__main__":
     main()
+
